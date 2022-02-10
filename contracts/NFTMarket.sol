a7444f7a4e4710af264dfae8fb656ea8b3da8b4a
Aoi <ht.so.ik0102@gmail.com>
2022-02-10 07:57:35 +0700

    createMarketPrice

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -80,4 +80,27 @@ contract NFTMarket is ReentrancyGuard {
             false
         );
     }
+
+    function createMarketSale(address nftContract, uint256 itemId)
+        public
+        payable
+        nonReentrant
+    {
+        uint256 price = idToMarketItem[itemId].price;
+        uint256 tokenId = idToMarketItem[itemId].tokenId;
+        require(
+            msg.value == price,
+            "Please submit the asking price"
+        );
+
+        idToMarketItem[itemId].owner = payable(msg.sender);
+        idToMarketItem[itemId].sold = true;
+
+        idToMarketItem[itemId].seller.transfer(msg.value);
+
+        IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId);
+
+        _itemsSold.increment();
+        payable(owner).transfer(listingPrice);
+    }
 }
