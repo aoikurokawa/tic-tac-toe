4330ac7d079279a4ec7d028980c0366ed878390e
Aoi <ht.so.ik0102@gmail.com>
2022-02-13 22:57:56 +0700

    chage

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { cp } from 'fs';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <nav className='border-b p-6'>
+        <p className='text-4xl font-bold'>Metaberse Marketplace</p>
+        <div className='flex mt-4'>
+          
+        </div>
+     </nav>
+     
     </div>
   );
 }
