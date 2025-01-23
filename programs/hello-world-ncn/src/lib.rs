0e30d492342eed534be42199f5abe29cae795cb5
aoikurokawa <ht.so.ik0102@gmail.com>
2025-01-23 20:39:54 +0900

    feat: update

--- programs/hello-world-ncn/src/lib.rs
+++ programs/hello-world-ncn/src/lib.rs
@@ -1,16 +1,27 @@
 use anchor_lang::prelude::*;
 
+use instructions::{play, setup_game};
+
+pub mod core;
+pub mod instructions;
+
 declare_id!("5trraE6UJC9m6TKDRQQkXoC3VaFYGYnzKeTwyfXXjho7");
 
 #[program]
 pub mod hello_world_ncn {
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn setup_game(
+        ctx: Context<setup_game::SetupGame>,
+        player_two: Pubkey,
+    ) -> Result<()> {
+        msg!("Instruction: SetupGame");
+        instructions::setup_game::setup_game(ctx, player_two)
+    }
+
+    pub fn play(ctx: Context<play::Play>, tile: Tile) -> Result<()> {
+        msg!("Instruction: Play");
+        instructions::play::play(ctx, player_two)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
